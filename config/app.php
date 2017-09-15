044d04600a22241b598415a00ad0bf950b88a96e
Atobajaiye Boluwaji <genmax100.ab@gmail.com>
2017-09-15 19:54:05 +0100

    bug fixes

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://serene-hamlet-38758.herokuapp.com/'),
 
     /*
     |--------------------------------------------------------------------------
